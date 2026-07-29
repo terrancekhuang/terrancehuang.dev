3879b8b18c5265fb6dc3a6e54ad93c0010c88ac3
Terrance Huang <terrancehuang@proton.me>
2026-07-29 01:23:09 -0400

    Revert "changed extracurricular content"
    This reverts commit 221f2ae031882abd8c9bfc14e996d9ad2b5432ab.

--- src/data/content.ts
+++ src/data/content.ts
@@ -51,7 +51,7 @@ export const extracurriculars: ExperienceEntry[] = [
   },
   {
     role: "President",
-    org: "Brothers and Sisters in Christ (BASIC) at Rochester Institute of Technology",
+    org: "Brothers and Sisters in Christ (BASIC) at RIT",
     dates: "January 2026 – May 2026",
   },
 ];
